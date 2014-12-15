1730bfed36a27cb5e319562a7b2d965e9896d971
Kalen Brunham <kalen@brunham.net>
2014-12-14 22:24:06 -0500

    Fixed typo in machine rule under linux

--- build/machine.sh
+++ build/machine.sh
@@ -7,6 +7,6 @@ echo "Creating machine info for $uname"
 if [ $uname = "Darwin" ]; then
     sysctl -n machdep.cpu.brand_string | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/(R)//' -e 's/CPU//' -e 's/@/ /'| sed -E 's/^[[:space:]]+//g' | sed -E 's/[[:space:]]+/_/g' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
 else
-    grep -m 1 "model name" /proc/cpuinfo | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/(R)//' -e 's/CPU//' -e 's/@/ /'| sed -E 's/^[[:space:]]+//g' | sed -E 's/[[:space:]]+/_/g' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
+    grep -m 1 "model name" /proc/cpuinfo | sed 's/model name.*: //' | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/(R)//' -e 's/CPU//' -e 's/@/ /'| sed -E 's/^[[:space:]]+//g' | sed -E 's/[[:space:]]+/_/g' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
 fi
 cat ../src/machine.h
