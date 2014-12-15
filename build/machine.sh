e6b48b9446519df3e87c4ceabaaacb2371202d27
Kalen Brunham <kalen@brunham.net>
2014-12-14 22:19:29 -0500

    Updated script to get machine name to remove (R). Updated linux rule to be the same as mac.

--- build/machine.sh
+++ build/machine.sh
@@ -5,8 +5,8 @@ uname=`uname -s`
 echo "Creating machine info for $uname"
 
 if [ $uname = "Darwin" ]; then
-    sysctl -n machdep.cpu.brand_string | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/CPU//' -e 's/@/ /'| sed -E 's/^[[:space:]]+//g' | sed -E 's/[[:space:]]+/_/g' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
+    sysctl -n machdep.cpu.brand_string | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/(R)//' -e 's/CPU//' -e 's/@/ /'| sed -E 's/^[[:space:]]+//g' | sed -E 's/[[:space:]]+/_/g' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
 else
-    grep -m 1 "model name" /proc/cpuinfo | sed 's/model name.*: //' | sed -r 's/\s+//g' | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/CPU//' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
+    grep -m 1 "model name" /proc/cpuinfo | sed -e 's/Intel(R)//' -e 's/(TM)//' -e 's/(R)//' -e 's/CPU//' -e 's/@/ /'| sed -E 's/^[[:space:]]+//g' | sed -E 's/[[:space:]]+/_/g' | sed 's/.*/#define MACHINE "&"/' > ../src/machine.h
 fi
 cat ../src/machine.h
