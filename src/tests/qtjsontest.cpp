66c01cf972224e6715bc31f54737fb5efcb34d4e
Kalen Brunham <kalen@brunham.net>
2015-01-10 02:06:07 -0500

    Initial version

---
+++ src/tests/qtjsontest.cpp
@@ -0,0 +1,151 @@
+/*
+ * Qt5 JSON test
+ *
+ *  Created on: Jan 10, 2015
+ *      Author: kbrunham
+ */
+
+#include "../test.h"
+
+#include <string>
+#include <QtCore/QtCore>
+#include <QtCore/QJsonDocument>
+#include <QtCore/QJsonObject>
+#include <QtCore/QJsonArray>
+
+static void GenStat(Stat& stat, const QJsonValue& v) {
+    if (v.isObject())
+	{
+		for (auto itr : v.toObject().keys()) {
+			stat.stringLength += itr.size();
+			GenStat(stat, v.toObject()[itr]);
+		}
+		stat.objectCount++;
+		stat.memberCount +=  v.toObject().size();
+		stat.stringCount +=  v.toObject().size();
+	}
+    else if (v.isArray())
+	{
+		for (auto o : v.toArray())
+		{
+			GenStat(stat, o);
+		}
+		stat.arrayCount++;
+		stat.elementCount += v.toArray().size();
+	}
+    else if (v.isString())
+    {
+		stat.stringCount++;
+		stat.stringLength += v.toString().size();
+    }
+    else if (v.isDouble())
+    {
+        stat.numberCount++;
+    }
+    else if (v.isBool())
+    {
+    	if (v.toBool())
+            stat.trueCount++;
+        else
+            stat.falseCount++;
+    }
+    else if (v.isNull())
+    {
+        stat.nullCount++;
+    }
+
+}
+
+static void GenStat(Stat& stat, const QJsonDocument& v) {
+    if (v.isObject())
+	{
+		for (auto itr : v.object().keys()) {
+			stat.stringLength += itr.size();
+			GenStat(stat, v.object()[itr]);
+		}
+		stat.objectCount++;
+		stat.memberCount +=  v.object().size();
+		stat.stringCount +=  v.object().size();
+	}
+    else if (v.isArray())
+	{
+		for (auto o : v.array())
+		{
+			GenStat(stat, o);
+		}
+		stat.arrayCount++;
+		stat.elementCount += v.array().size();
+	}
+}
+
+class QtJsonParseResult : public ParseResultBase {
+public:
+	QJsonDocument root;
+
+	QtJsonParseResult() = default;
+
+	~QtJsonParseResult() = default;
+
+};
+
+class QtJsonStringResult : public StringResultBase {
+public:
+    virtual const char* c_str() const { return s.c_str(); }
+
+    std::string s;
+};
+
+class QTJsonTest : public TestBase {
+public:
+#if TEST_INFO
+    virtual const char* GetName() const override { return "Qt Json (C++)"; }
+    virtual const char* GetFilename() const override { return __FILE__; }
+    virtual unsigned long GetMaxSize() const override {return 2500000;}
+#endif
+
+#if TEST_PARSE
+    virtual ParseResultBase* Parse(const char* json, size_t length) const override {
+        QtJsonParseResult* pr = new QtJsonParseResult;
+
+        pr->root = QJsonDocument::fromJson(json);
+
+    	return pr;
+    }
+#endif
+
+#if TEST_STRINGIFY
+    virtual StringResultBase* Stringify(const ParseResultBase* parseResult) const override {
+        const QtJsonParseResult* pr = static_cast<const QtJsonParseResult*>(parseResult);
+        QtJsonStringResult* sr = new QtJsonStringResult;
+
+        sr->s = pr->root.toJson(QJsonDocument::Compact).toStdString().c_str();
+
+        return sr;
+    }
+#endif
+
+#if TEST_PRETTIFY
+    virtual StringResultBase* Prettify(const ParseResultBase* parseResult) const {
+        const QtJsonParseResult* pr = static_cast<const QtJsonParseResult*>(parseResult);
+        QtJsonStringResult* sr = new QtJsonStringResult;
+
+        sr->s = pr->root.toJson(QJsonDocument::Indented).toStdString().c_str();
+
+        return sr;
+    }
+#endif
+
+
+#if TEST_STATISTICS
+    virtual bool Statistics(const ParseResultBase* parseResult, Stat* stat) const override {
+        const QtJsonParseResult* pr = static_cast<const QtJsonParseResult*>(parseResult);
+        memset(stat, 0, sizeof(Stat));
+        GenStat(*stat, pr->root);
+        return true;
+    }
+#endif
+};
+
+REGISTER_TEST(QTJsonTest);
+
+
